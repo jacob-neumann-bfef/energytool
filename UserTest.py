dd9924adf0544fbdcf7f470f999052db6bc644c6
jneumann <jacob.neumann@bfef.biz>
2015-11-12 02:33:42 -0500

    First Commit

---
+++ UserTest.py
@@ -0,0 +1,11 @@
+sector= raw_input('Which sector will the program target? ')
+margcredit= raw_input('Will the program target borrowers with marginal credit? ')
+subfin= raw_input('Will the program offer subsidized financing? ')
+tenant= raw_input('Will the target market include tenant-occupied buildings? ')
+nonrepayfunds= raw_input('Do you have access to funds that will not need to be repaid? ')
+adddebt= raw_input('Can you take on additional debt? ')
+bonds= raw_input('Can you issue bonds to support this particular program? ')
+obr= raw_input('Is on-bill repayment through local utilities a viable option? ')
+state= raw_input('What State do you live in? ')
+
+
